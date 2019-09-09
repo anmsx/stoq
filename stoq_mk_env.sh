d1b7458f5871094b6425099cea4689864cf7f66e
anmsx <slv.eziel@gmail.com>
2019-09-09 13:32:25 -0300

    script

--- stoq_mk_env.sh
+++ stoq_mk_env.sh
@@ -1,7 +1,7 @@
-user="$USER"
+user="tds" # "$USER"
 p="$HOME/stoq-dev-bkp"
 pgit="$HOME/stoq-dev-git"
-pgit="$HOME/stoqgithub/stoq"
+pgit="$HOME/stoq/stoq"
 #gitrep="http://gitlab.com/anmsx/stoq_src.git"
 gitrep="http://github.com/anmsx/stoq.git"
 
@@ -14,15 +14,17 @@ mkdir $p
 
 mv /usr/share/stoq "$p/stoq"
 mv /usr/share/stoqserver "$p/stoqserver"
-mv /usr/local/lib/python3.6/dist-packages/stoq-3.2.0rc1-py3.6.egg/stoqlib "$p/stoqlibdist"
-mv /usr/local/lib/python3.6/dist-packages/stoq-3.2.0rc1-py3.6.egg/stoq    "$p/stoq_lib"
+mv /usr/lib/python3.6/dist-packages/stoqlib "$p/stoqlibdist"
+mv /usr/lib/python3.6/dist-packages/stoq    "$p/stoq_lib"
 
 #git clone $gitrep $pgit
 
 ln -s "$pgit/data"        /usr/share/stoq 
 #ln -s "$pgit/stoqserver"  /usr/share/stoqserver 
-ln -s "$pgit/stoqlib"     /usr/local/lib/python3.6/dist-packages/stoq-3.2.0rc1-py3.6.egg/stoqlib 
-ln -s "$pgit/stoq"        /usr/local/lib/python3.6/dist-packages/stoq-3.2.0rc1-py3.6.egg/stoq 
+#ln -s "$pgit/stoqlib"     /usr/lib/python3.6/dist-packages/stoq-3.1.0.egg-info/stoqlib 
+#ln -s "$pgit/stoq"        /usr/lib/python3.6/dist-packages/stoq-3.1.0.egg-info/stoq 
+ln -s "$pgit/stoqlib"     /usr/lib/python3.6/dist-packages/stoqlib 
+ln -s "$pgit/stoq"        /usr/lib/python3.6/dist-packages/stoq 
 
 
 chown -R $user $pgit/*
